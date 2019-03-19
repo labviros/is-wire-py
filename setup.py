2eee616949c322352a670679cda8f713ba7b77f9
Felippe <mendonca.felippe@gmail.com>
2019-03-19 17:37:30 -0300

    version 1.1.4 released

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='is_wire',
-    version='1.1.3',
+    version='1.1.4',
     description='',
     url='http://github.com/labviros/is-wire-py',
     author='labviros',
